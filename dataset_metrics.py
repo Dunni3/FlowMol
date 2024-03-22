d03cd16a9384519e1303a74daff78d4f141e5c6c
Ian Dunn <ian@iandunn.io>
2024-03-22 17:19:26 -0400

    batch wise dataset metics

--- dataset_metrics.py
+++ dataset_metrics.py
@@ -10,6 +10,8 @@ import numpy as np
 import pickle
 from tqdm import tqdm
 from data_processing.utils import get_upper_edge_mask
+import math
+from collections import defaultdict
 
 # disable rdkit logging
 from rdkit.Chem import AllChem as Chem
@@ -19,6 +21,7 @@ RDLogger.DisableLog('rdApp.*')
 def parse_args():
     p = argparse.ArgumentParser(description='Dataset Metrics')
     p.add_argument('--config', type=Path, required=True, help='Path to config file')
+    p.add_argument('--batch_size', type=int, default=None)
     return p.parse_args()
 
 def dataset_to_mols(train_dataset, atom_type_map) -> List[SampledMolecule]:
@@ -59,11 +62,48 @@ if __name__ == "__main__":
     # create sample analyzer
     sample_analyzer = SampleAnalyzer()
 
-    # convert the training dataset to a list of SampledMolecule objects
-    mols = dataset_to_mols(train_dataset, config['dataset']['atom_map'])
+    
+    if args.batch_size:
+        n_batches = math.ceil(len(train_dataset) / args.batch_size)
+        energies = []
+        batch_metrics = []
+        for i in range(n_batches):
+            start = i * args.batch_size
+            end = min(start + args.batch_size, len(train_dataset))
+            mols = [ train_dataset[datset_idx] for dataset_idx in range(start, end)]
+            mols = dataset_to_mols(mols, config['dataset']['atom_map'])
+            energies.extend(sample_analyzer.compute_sample_energy(mols))
+            batch_metrics.append(sample_analyzer.analyze(mols, return_counts=True))
 
-    # compute the UFF energies for the dataset
-    energies = sample_analyzer.compute_sample_energy(mols)
+        # compute metrics on the sampled molecules
+        numerators = defaultdict(float)
+        denominators = defaultdict(float)
+        for count_dict in batch_metrics:
+            numerators['frac_atoms_stable'] += count_dict['n_stable_atoms']
+            numerators['frac_mols_stable_valence'] += count_dict['n_stable_molecules']
+            numerators['frac_valid_mols'] += count_dict['n_valid']
+            numerators['avg_frag_frac'] += count_dict['sum_frag_fracs']
+            numerators['avg_num_components'] += count_dict['sum_num_components']
+
+            denominators['frac_atoms_stable'] += count_dict['n_atoms']
+            denominators['frac_mols_stable_valence'] += count_dict['n_molecules']
+            denominators['frac_valid_mols'] += count_dict['n_molecules']
+            denominators['avg_frag_frac'] += count_dict['n_frag_fracs']
+            denominators['avg_num_components'] += count_dict['n_num_components']
+        
+        metrics = {}
+        for key in numerators.keys():
+            metrics[key] = numerators[key] / denominators[key]
+
+    else:
+        # convert the training dataset to a list of SampledMolecule objects
+        mols = dataset_to_mols(train_dataset, config['dataset']['atom_map'])
+
+        # compute the energies for the dataset
+        energies = sample_analyzer.compute_sample_energy(mols)
+
+        # compute metrics on the sampled molecules
+        metrics = sample_analyzer.analyze(mols)
 
     # compute a discrete distribution of energies
     bins = np.linspace(-200, 500, 200)
@@ -75,8 +115,7 @@ if __name__ == "__main__":
     energy_dist_file = processed_data_dir / 'energy_dist.npz'
     save_reference_dist(bins, p_dataset, energy_dist_file)
 
-    # compute metrics on the sampled molecules
-    metrics = sample_analyzer.analyze(mols)
+    # write metrics
     metrics_file = processed_data_dir / 'metrics.pkl'
     with open(metrics_file, 'wb') as f:
         pickle.dump(metrics, f)
