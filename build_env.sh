65794a99fc5bd86451f812597ab794d7420144a9
Ian Dunn <ian@iandunn.io>
2024-09-22 13:10:38 -0400

    modify env spec

--- build_env.sh
+++ build_env.sh
@@ -15,4 +15,4 @@ mamba install pytorch-cluster=1.6.3 pytorch-scatter=2.1.2 -c pyg -y
 mamba install -c dglteam/label/cu121 dgl=2.0 -y
 mamba install -c conda-forge pytorch-lightning=2.1.0 -y
 mamba install -c conda-forge rdkit=2023.09.1 pystow -y
-pip install wandb useful_rdkit_utils --no-input
+pip install wandb useful_rdkit_utils torch-ema --no-input
