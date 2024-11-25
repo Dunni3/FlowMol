79453d5a0638cb5db02a7c068bc09576c1b7ef85
Ian Dunn <ian@iandunn.io>
2024-11-25 12:41:46 -0500

    env test fixes

--- build_env.sh
+++ build_env.sh
@@ -8,7 +8,7 @@ ENV_NAME=$(basename "$CONDA_PREFIX")
 echo "Building flowmol into the environment '$ENV_NAME'"
 
 mamba install pytorch=2.2.0 torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia -y
-mamba install pytorch-cluster=1.6.3 pytorch-scatter=2.1.2 -c pyg -y
+mamba install pytorch-cluster=1.6.3 pytorch-scatter=2.1.2=py310_torch_2.2.0_cu121 -c pyg -y
 mamba install -c dglteam/label/cu121 dgl=2.0 -y
 mamba install -c conda-forge pytorch-lightning=2.1.3 -y
 mamba install -c conda-forge rdkit=2023.09.4 pystow -y
