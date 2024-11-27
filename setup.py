96278839de037aa92affc8a89586994f05fb6953
Ian Dunn <ian@iandunn.io>
2024-11-27 09:34:56 -0500

    added setup.py

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ setup(
     python_requires=">=3.10,<3.11",
     packages=find_packages(),
     install_requires=[
-        "torch==2.2.0 @ https://download.pytorch.org/whl/cu121",
-        "torchvision @ https://download.pytorch.org/whl/cu121",
-        "torchaudio @ https://download.pytorch.org/whl/cu121",
+        "torch @ https://download.pytorch.org/whl/cu121/torch-2.2.0%2Bcu121-cp310-cp310-linux_x86_64.whl",
+        "torchvision @ https://download.pytorch.org/whl/cu121/torchvision",
+        "torchaudio @ https://download.pytorch.org/whl/cu121/torchaudio",
         "torch-cluster==1.6.3",
         "torch-scatter==2.1.2",
         "dgl @ https://data.dgl.ai/wheels/torch-2.2/cu121/dgl-2.2.0%2Bcu121-cp310-cp310-manylinux1_x86_64.whl",
