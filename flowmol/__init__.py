e6b16604b6a13aef95aa4e2813a0017dec489341
Ian Dunn <ian@iandunn.io>
2024-12-02 12:18:42 -0500

    finished implementing+testing models-on-demand

--- flowmol/__init__.py
+++ flowmol/__init__.py
@@ -35,9 +35,16 @@ def load_pretrained(model_name: str) -> FlowMol:
     if not model_dir.exists():
         # download the model if it doesn't exist
         download_remote_model_dir(model_dir)
+
+    if 'qm9' in model_name:
+        marginal_dists_file = Path(__file__).parent.parent / 'data' / 'qm9' / 'train_data_marginal_dists.pt'
+        n_atoms_hist_file = Path(__file__).parent.parent / 'data' / 'qm9' / 'train_data_n_atoms_histogram.pt'
+        load_kwargs = {'marginal_dists_file': marginal_dists_file, 'n_atoms_hist_file': n_atoms_hist_file}
+    else:
+        load_kwargs = {}
     
     ckpt_path = model_dir / 'checkpoints' / 'last.ckpt'
-    model = FlowMol.load_from_checkpoint(ckpt_path)
+    model = FlowMol.load_from_checkpoint(ckpt_path, **load_kwargs)
 
     if model_name == 'geom_ctmc':
         # temporary fix - this model was trained with certain
@@ -46,6 +53,7 @@ def load_pretrained(model_name: str) -> FlowMol:
         # to what we've found to give better results 
         model.vector_field.eta = 30.0
         model.vector_field.hc_thresh = 0.9
+        
 
     return model
 
@@ -53,19 +61,19 @@ def download_remote_model_dir(local_model_dir: Path):
 
     print("Downloading pretrained model...")
 
-    # local_model_dir
-    local_model_dir.mkdir(exist_ok=True)
-
     # make local_model_dir an absolute path
     local_model_dir = local_model_dir.resolve()
+
+    # get download location
+    local_download_path = local_model_dir.parent
+    local_download_path = local_download_path.resolve()
 
     # get location of remote model dir
     model_name = local_model_dir.name
     remote_model_dir = f"https://bits.csb.pitt.edu/files/FlowMol/trained_models/{model_name}/"
 
     # download the model
-    wget_cmd = f"wget -r -np -nH --cut-dirs=2 --reject 'index.html*' -P {local_model_dir} {remote_model_dir}"
+    wget_cmd = f"wget -r -np -nH --cut-dirs=3 --reject 'index.html*' -P {local_download_path} {remote_model_dir}"
     result = subprocess.run(wget_cmd, shell=True, capture_output=True, text=True)
     if result.returncode != 0:
         raise RuntimeError(f"Error downloading model: {result.stderr}")
-    print(result.stdout)
