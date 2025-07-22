a30d9d5cf12c8614f1225eb069a95874e607bc17
Ian Dunn <ian@iandunn.io>
2025-07-22 14:50:41 -0400

    minor changes to evals

--- dataset_metrics.py
+++ dataset_metrics.py
@@ -26,7 +26,7 @@ def parse_args():
 
     return p.parse_args()
 
-def dataset_to_mols(train_dataset, atom_type_map) -> List[SampledMolecule]:
+def dataset_to_mols(train_dataset, atom_type_map, fake_atoms) -> List[SampledMolecule]:
     print('converting dataset from graphs to molecules')
     mols = []
     for g in tqdm(train_dataset, mininterval=15):
@@ -39,7 +39,7 @@ def dataset_to_mols(train_dataset, atom_type_map) -> List[SampledMolecule]:
             data_src[f'{feat}_1'] = data_src[f'{feat}_1_true']
 
         g.edata['ue_mask'] = get_upper_edge_mask(g)
-        sampled_mol = SampledMolecule(g, atom_type_map)
+        sampled_mol = SampledMolecule(g, atom_type_map, fake_atoms=fake_atoms)
         mols.append(sampled_mol)
 
     return mols
@@ -51,6 +51,9 @@ if __name__ == "__main__":
 
     # read config file
     config: dict = read_config_file(args.config)
+
+    # determine whether there are fake atoms
+    fake_atoms = config['mol_fm'].get('fake_atom_p', 0.0) > 0.0
 
     # get the training dataset
     data_module = data_module_from_config(config)
@@ -64,7 +67,7 @@ if __name__ == "__main__":
     # create sample analyzer
     processed_data_dir = config['dataset']['processed_data_dir']
     processed_data_dir = Path(processed_data_dir)
-    sample_analyzer = SampleAnalyzer(processed_data_dir=processed_data_dir)
+    sample_analyzer = SampleAnalyzer(processed_data_dir=processed_data_dir, pb_energy=True)
 
     if args.n_mols is not None:
         # randomly select n_mols numbers from the range (0, len(dataset))
@@ -74,64 +77,46 @@ if __name__ == "__main__":
     # compute metrics
     if args.batch_size:
         n_batches = math.ceil(len(train_dataset) / args.batch_size)
-        energies = []
         batch_metrics = []
         for i in range(n_batches):
             start = i * args.batch_size
             end = min(start + args.batch_size, len(train_dataset))
             mols = [ train_dataset[dataset_idx] for dataset_idx in range(start, end)]
-            mols = dataset_to_mols(mols, config['dataset']['atom_map'])
-            energies.extend(sample_analyzer.compute_sample_energy(mols))
-            batch_metrics.append(sample_analyzer.analyze(mols, return_counts=True))
+            mols = dataset_to_mols(mols, config['dataset']['atom_map'], fake_atoms=fake_atoms)
+            # energies.extend(sample_analyzer.compute_sample_energy(mols))
+            metrics = sample_analyzer.analyze(mols, posebusters=True)
+            metrics['n_mols'] = len(mols)
+            batch_metrics.append(metrics)
 
-        # compute metrics on the sampled molecules
+        # compute aggregated metrics
+        metrics_to_aggregate = [metric_name for metric_name in metrics.keys() if 'pb_' in metric_name]
+        metrics_to_aggregate.extend(['frac_valid_mols', 'frac_connected'])
+
         numerators = defaultdict(float)
-        denominators = defaultdict(float)
-        for count_dict in batch_metrics:
-            numerators['frac_atoms_stable'] += count_dict['n_stable_atoms']
-            numerators['frac_mols_stable_valence'] += count_dict['n_stable_molecules']
-            numerators['frac_valid_mols'] += count_dict['n_valid']
-            numerators['avg_frag_frac'] += count_dict['sum_frag_fracs']
-            numerators['avg_num_components'] += count_dict['sum_num_components']
+        denominators = defaultdict(int)
 
-            denominators['frac_atoms_stable'] += count_dict['n_atoms']
-            denominators['frac_mols_stable_valence'] += count_dict['n_molecules']
-            denominators['frac_valid_mols'] += count_dict['n_molecules']
-            denominators['avg_frag_frac'] += count_dict['n_frag_fracs']
-            denominators['avg_num_components'] += count_dict['n_num_components']
+        for metrics_dict in batch_metrics:
+            for metric_name in metrics_to_aggregate:
+                numerators[metric_name] += metrics_dict[metric_name]*metrics_dict['n_mols']
+                denominators[metric_name] += metrics_dict['n_mols']
         
-        metrics = {}
-        for key in numerators.keys():
-            metrics[key] = numerators[key] / denominators[key]
+        output_metrics = { metric_name: numerators[metric_name]/denominators[metric_name] for metric_name in numerators }
+
 
     else:
         # convert the training dataset to a list of SampledMolecule objects
         mols = dataset_to_mols(train_dataset, config['dataset']['atom_map'])
 
-        # compute the energies for the dataset
-        energies = sample_analyzer.compute_sample_energy(mols)
-
         # compute metrics on the sampled molecules
-        metrics = sample_analyzer.analyze(mols)
-
-    # compute a discrete distribution of energies
-    bins = np.linspace(-200, 500, 200) # this range of bins captures ~99% of the density for the MMFF energies of both QM9 and GEOM-DRUGS datasets -- is that reasonable?
-    counts_dataset, _ = np.histogram(energies, bins=bins, density=False)
-    # compute the fraction of the molecules which fall outside these bins
-    frac_outside = 1 - counts_dataset.sum() / len(energies)
-    # print the fraction of the molecules which fall outside the bins
-    print(f'fraction of molecules outside the bins: {frac_outside:.4f}')
-    p_dataset = counts_dataset / len(energies)
+        output_metrics = sample_analyzer.analyze(mols, posebusters=True)
 
     # save the reference distribution
     processed_data_dir = Path(config['dataset']['processed_data_dir'])
-    energy_dist_file = processed_data_dir / 'energy_dist.npz'
-    save_reference_dist(bins, p_dataset, energy_dist_file)
 
     # write metrics
-    metrics_file = processed_data_dir / 'metrics.pkl'
+    metrics_file = processed_data_dir / 'metrics_fm3.pkl'
     with open(metrics_file, 'wb') as f:
-        pickle.dump(metrics, f)
+        pickle.dump(output_metrics, f)
 
     # print metrics
     for k, v in metrics.items():
